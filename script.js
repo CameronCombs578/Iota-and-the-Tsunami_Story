fa0b8f683497ee3143848a7cd03f8bcfa1ec8a3c
CameronCombs578 <camcombs@ttu.edu>
2026-04-16 16:05:01 -0500

    feat: added images and continuation of chapter 1

--- script.js
+++ script.js
@@ -16,13 +16,23 @@ startButton.addEventListener("click", () => {
 
     // Replace the placeholder text with the first scene
     playContent.innerHTML = `
-        <div class="Chapter1">
-            <div class="Iota-image">
-                <img src="images/Iota_Paint.png" alt="Iota standing on the beach" />
-            </div> 
+
                    
             <div class="story-text">
+
+
                 <h2>Chapter 1: The Calm before the Storm</h2>
+                <div class="Chapter1">
+                    <div class="Iota-image">
+                        <img src="images/IotaAndText_Paint.png" alt="Iota standing" />
+                    </div> 
+                    <div class="village-image">
+                        <img src="images/Vilage.png" alt="Mushroom Village" />
+                    </div> 
+                    <div class="Grace-image">
+                        <img src="images/GraceAndText_Paint.png" alt="Grace standing" />
+                    </div> 
+                </div>
                 <p>It was a calm morning at Mushroom Village and the sun was shining bring upon the land. The vilagers were going about their daily tasks. Some were farming other gardening. Kids were studying hard at the Vilage's school. One girl, named Iota, had just finished her assignments for that day and was walking home.</p>
                 <p>As she was walking, she thought about her day of school and reflected upon what she had learned. "Pride is destructive," she thought to herself. "The Teachers story of pride consuming a person the inside out is really hard to imagine though." She hasented to Grace's house, one of her best friends, to see what she was doing.</p>
                 <button id="continueButton">Continue</button>
@@ -38,8 +48,28 @@ startButton.addEventListener("click", () => {
 
     continueButton.addEventListener("click", () => {
         playContent.innerHTML = `
-            <h2>Chapter 1: The Calm before the Storm Continued</h2>
-            <p>To be continued.</p>
+            <div class="story-text">
+
+
+                <h2>Chapter 1 Continued: The Calm before the Storm</h2>
+                <div class="Chapter1">
+                    <div class="Iota-image">
+                        <img src="images/IotaAndText_Paint.png" alt="Iota standing" />
+                    </div> 
+                    <div class="village-image">
+                        <img src="images/Vilage.png" alt="Mushroom Village" />
+                    </div> 
+                    <div class="Grace-image">
+                        <img src="images/GraceAndText_Paint.png" alt="Grace standing" />
+                    </div> 
+                </div>
+                <p>"Hi Iota!" Grace said as she waved.</p>
+                <button id="continueButton">Hi Grace!</button>
+            </div>
+
+
+
+        </div>
             <button id="menuReturnButton">Return to Menu</button>
         `;
 
