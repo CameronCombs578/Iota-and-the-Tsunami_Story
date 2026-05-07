e2aed65396a9235c0fb19b7880a073838a6ea74c
CameronCombs578 <camcombs@ttu.edu>
2026-05-07 14:59:41 -0500

    feat: added first draft for chapter 4

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ function showScene3() {
 function showCleaningScene() {
     playContent.innerHTML = `
         <div class="story-text">
-            <h2>Chapter 1 Continued: Helping with Cleaning</h2>
+            <h2>Chapter 1 Continued: The Calm before the Storm</h2>
             <div class="Chapter1">
                 <div class="Iota-image">
                     <img src="images/IotaAndText_Paint.png" alt="Iota standing" />
@@ -134,7 +134,7 @@ function showCleaningScene() {
 function showGardeningScene() {
     playContent.innerHTML = `
         <div class="story-text">
-            <h2>Chapter 1 Continued: Helping with Gardening</h2>
+            <h2>Chapter 1 Continued: The Calm before the Storm</h2>
             <div class="Chapter1">
                 <div class="Iota-image">
                     <img src="images/IotaAndText_Paint.png" alt="Iota standing" />
@@ -190,8 +190,8 @@ function showScene6Path1() {
                 <div class="Iota-image">
                     <img src="images/IotaAndText_Paint.png" alt="Iota standing" />
                 </div>
-                <div class="village-image">
-                    <img src="images/Vilage.png" alt="Mushroom Village" />
+                <div class="shore_Paint">
+                    <img src="images/Shore_Paint.png" alt="Shore" />
                 </div>
                 <div class="Grace-image">
                     <img src="images/GraceAndText_Paint.png" alt="Grace standing" />
@@ -235,7 +235,7 @@ function showScene6Path2() {
 function showScene7Path1() {
     playContent.innerHTML = `
         <div class="story-text">
-            <h2>Chapter 3 Continued: The Wall of Water</h2>
+            <h2>Chapter 3: The Wall of Water</h2>
             <div class="Chapter3">
                 <div class="Iota-image">
                     <img src="images/IotaAndText_Paint.png" alt="Iota standing" />
@@ -307,7 +307,7 @@ function showScene8Route1Path1() {
 function showScene9Route1Path1() {
     playContent.innerHTML = `
         <div class="story-text">
-            <h2>Chapter 3 Continued: The Wall of Water</h2>
+            <h2>Chapter 4: Get to Safety</h2>
             <div class="Chapter3">
                 <div class="Iota-image">
                     <img src="images/IotaAndText_Paint.png" alt="Iota standing" />
@@ -323,13 +323,66 @@ function showScene9Route1Path1() {
                 </div>
             </div>
             <p>"Seems to be." the stranger began to say. "Here I was walking and minding my own business when I here someone shouting for help. When I got here I see you two. I would help, but my parents taught me to not talk to strangers." "But we are not strangers, you are a part of Mushroom Village right?" Iota said. "Well, I guess, but you could have guessed. How can I realy know if you are apart of the vilage?" The boy said suspiciously. "I don't know, but we abviousely need help and I am telling the truth. Please help us." "How?" said the boy. "You can warn the village that there is an incomming tsunami." "A what!!!" The boy shouted shocked." "Yeah, a tsunami. Look out towards the sea with this scope."</p>
-            <p>The boy cautiously took the scope from Iota and looked through it to see the wall of water. "Oh my goddness, you two are telling the truth. I can go warn the villagers and get someone to help you two" the boy said as he ran off. "Thank you very much... uh what is your name?" Iota yelled. "Mark!" the boy said as he ran full speed throug the woods. "Thank you Mark!" Grace and Iota yelled as he disappeared into the trees.
-            <p>It took Mark a while to get to Mushroom village, but he was able to warn the villagers about the tsunami and get help for Grace. The villagers relocated their homes to a location out of the way of the tsunami. They all watch together as the wall of water reached land and detroyed all the trees in the way. "Thankfully we did not experience that" a vilager said. "I am so glad that we were warned abotu the tsunami, but there were a few villagers that did not relocate because of their businesses. They gave their life for thier own wealth." "Wow, people would do that," Iota thought to herself. "Well, I am grateful that many of the villagers herad our warning." "This is just like the story we heard for school today, pride consuming from the inside out." Grace said as she looked at the tsunami. "Thankfully we were able to work together to get alot of people to saftey," Mark said. "Yeah, good thing" Iota said quietly.</p>
-            <button id="menuButton">End</button>
+            <p>The boy cautiously took the scope from Iota and looked through it to see the wall of water. "Oh my goddness, you two are telling the truth. What can we do?" the boy said. "Warn the vilagers before it is too late," Grace said. Imeadiatley, the boy ran towards the vilage. "Thank you very much... uh what is your name?" Iota yelled. "Mark!" the boy said as he ran full speed through the woods. "Thank you Mark!" Grace and Iota yelled as he disappeared into the trees.
+            <button id="Chapter4continued">Continue</button>
         </div>
     `;
-    document.getElementById("menuButton").addEventListener("click", showMenu);
+    document.getElementById("Chapter4continued").addEventListener("click", showScene10Route1Path1);
+    /* document.getElementById("menuButton").addEventListener("click", showMenu);*/
 }
+
+function showScene10Route1Path1() {
+    playContent.innerHTML = `
+        <div class="story-text">
+            <h2>Chapter 4 Continued: Get to Safety</h2>
+            <div class="Chapter4">
+                <div class="Iota-image">
+                    <img src="images/IotaAndText_Paint.png" alt="Iota standing" />
+                </div>
+                <div class="village-image">
+                    <img src="images/Vilage.png" alt="Mushroom Village" />
+                </div>
+                <div class="Grace-image">
+                    <img src="images/GraceAndText_Paint.png" alt="Grace standing" />
+                </div>
+            </div>
+            <p>"Wait a second." Iota said. "What about us!" Iota began to yell for Mark, but he was out of sight. "Oh no oh no," Iota began to say say. "Its okay Iota, we can try to get to the mountains where it is safe." "How!?" Iota said. "I..uh..don't know." "We can't stay here. How is you leg fealing?" "Okay I guss," Grace said as she move it an inch from the ground" "I think I can make it with your help now." "Then we should go." Iota said. The two of them began to walk towards the mountains where they would be safe from the tsunami.
+            <button id="Chapter4option1">Return to the village</button>
+            <button id="Chapter4option2">Wait for Help</button>
+        </div>
+    `;
+    document.getElementById("Chapter4option1").addEventListener("click", showScene11Route1Path1);
+    /* document.getElementById("menuButton").addEventListener("click", showMenu);*/
+}
+
+function showScene11Route1Path1() {
+    playContent.innerHTML = `
+        <div class="story-text">
+            <h2>Chapter 4 Continued: Get to safety</h2>
+            <div class="Chapter4">
+                <div class="Mark-image">
+                    <img src="images/MarkAndText_Paint.png" alt="Mark standing" />
+                </div>
+                <div class="village-image">
+                    <img src="images/Vilage.png" alt="Mushroom Village" />
+                </div>
+                <div class="Crowd-image">
+                    <img src="images/CrowdAndText_Paint.png" alt="Crowd standing" />
+                </div>
+                    <div class="Jerry-image">
+                    <img src="images/Jerry_Paint.png" alt="Crowd standing" />
+                </div>
+            </div>
+            <p>Mark ran into the vailage and yelled from the top of his lungs, "TSUNAMI!" "What about a tsunami?" a villager said. "One is heading our way, we need to evacuate!" Mark replied. The croud began to patiently wait for more. "What?" Mark said. "Enough with the game boy, don't you have homework to do." "What? No! I am serious. Go to the shore and look with a scope towards the sea!" Mark said as he motioned the actions. "Hmmm..." "Please trust me... wait, not don't trust me, look for yourself!" Mark said as he raced to the shore. Only a few vilagers walked after him and were astonished when they saw the wall of water. "You were right after all," they said. Each vilager that witnessed the tsunami warned one of their freinds, which warned their friend. This process continued until half of the villagers were ready to move to the mountains while the other half were not.
+            <p>"You all are trusting in a boy's game while we are basing our action on evidence. "There is evedience, just look..." "We have heard the story but we still do not believe." "Why are they so resistant?" Mark said to one of the adults. "The half of villagers that are staying are the rich ones, they have businesses here and are not willing to leave." "Friends," Mr. Jerry said as he stood up on a box. "Do not listen to this boy and his games. If a real tsunami is coming we can stand it." The crowd yelled with support. "It is no use," Mark said, "ya your right. you cannot trick us." "He is not tricking you," "Who said that," Jerry said as he looked as Grace and Iota enter into the village. "After buying a scope, we sat by the shore and saw the tsunami coming. Please believe us." "Well... uh... you... you are probably making up this joke with him." Jerry thought aloud to support his case. "Mayber they are right," one of Jerry's supporters said as he joined the other half of villagers. "Well, you can go, but we are going stay," Jerry and his supportes said together. Iota, Grace, Mark, and half of the villagers walked towards the mountain.
+            <button id="Chapter4option1">Chapter 5</button>
+        </div>
+    `;
+    document.getElementById("Chapter4option1").addEventListener("click", showScene12Route1Path1);
+    /* document.getElementById("menuButton").addEventListener("click", showMenu);*/
+}
+
+
 
 /* Utility Functions */
 function showMenu() {
